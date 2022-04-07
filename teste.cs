0fc592fe280bf5dce670685b70540a7ef39fdea2
Guilherme Araújo <92218408+Garauj@users.noreply.github.com>
2022-04-06 21:40:38 -0300

    Add files via upload
    GET SET

---
+++ teste.cs
@@ -0,0 +1,16 @@
+﻿
+class Teste
+{
+    private double _preco;
+    public Teste() { }
+    public Teste(double preco) { _preco = preco; }
+
+    public double GetPreco()
+    {
+        return _preco * 10;
+    }
+    public void SetNome(double preco)
+    {
+                _preco = preco * 5;
+    }
+}
